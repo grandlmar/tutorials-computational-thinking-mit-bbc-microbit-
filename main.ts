55fb2ab9f45e2781fc325f4350fba55ec7fe0dbf
Maria Grandl <97896791+grandlmar@users.noreply.github.com>
2022-04-22 22:21:58 +0200

    blinking_heart

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-input.onGesture(Gesture.Shake, function () {
+for (let index = 0; index < 3; index++) {
     basic.showIcon(IconNames.Heart)
-})
+    basic.pause(500)
+    basic.clearScreen()
+    basic.pause(500)
+}
